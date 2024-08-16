8637470b58ef01ffb0bfa4d2d9140d9765dacf18
Axel Demborg <axel.demborg@gmail.com>
2024-08-17 00:39:59 +0200

    add shitty screen controls

--- UI.gd
+++ UI.gd
@@ -3,3 +3,27 @@ extends Control
 func set_weight(w):
 	
 	$Scale.text = "Weight:\n" + str(round(100 * w) / 100) + "kg"
+
+func _on_Up_button_down():
+	Input.action_press("ui_up")
+
+func _on_Up_button_up():
+	Input.action_release("ui_up")
+
+func _on_Down_button_down():
+	Input.action_press("ui_down")
+
+func _on_Down_button_up():
+	Input.action_release("ui_down")
+	
+func _on_Left_button_down():
+	Input.action_press("ui_left")
+
+func _on_Left_button_up():
+	Input.action_release("ui_left")
+	
+func _on_Right_button_down():
+	Input.action_press("ui_right")
+
+func _on_Right_button_up():
+	Input.action_release("ui_right")
